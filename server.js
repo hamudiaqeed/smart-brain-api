0f63cde84bdb4820ea920601939c76d8b3d25cf8
Mohammed Aqeed <hamudy_fun5b@yahoo.com>
2020-04-04 19:10:09 +0300

    fixing server

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'test',
+        database: 'smart_brain'
     }
 });
 
@@ -24,7 +26,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('it is working!');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
@@ -37,7 +39,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
 })
 
